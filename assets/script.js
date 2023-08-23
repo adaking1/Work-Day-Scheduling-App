d89ed78c49999a5de38177fc7157440027262319
adaking1 <137830553+adaking1@users.noreply.github.com>
2023-08-23 13:22:07 -0400

    added comments and fixed logic for beginning of day reset

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,5 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-
+// this function saves text that was input in a calendar block. 
+// if the save button is pressed with no text, the fuction will return
 function pressSave() {
     var saveButton = $(".saveBtn");
     saveButton.on("click", function(){
@@ -15,20 +13,19 @@ function pressSave() {
     
 }
 
+
+// this function sets the text of the calendar if there was text saved in local storage
+
 function textSet() {
     $(".time-block").each(function() {
-        console.log($("#currentDay").text().slice(-10,-8));
-        console.log(dayjs().format("D"));
-        if ($("#currentDay").text().slice(-10,-8) !== dayjs().format("D")) {
-            localStorage.removeItem(this.id);
-        }
-
         if (localStorage.getItem(this.id) !== null) {
             this.children[1].textContent = localStorage.getItem(this.id);
         }
     })
 }
 
+// this function sets the date in the header
+// it also sets the day in local storage so when the day changes, the local storage clears. The calendar is clear for each new day.
 function dateSet() {
     var day = dayjs().format("D");
     var suffix = "";
@@ -50,9 +47,15 @@ function dateSet() {
     $("#currentDay").append(suffix);
     $("#currentDay").append(dayjs().format(", YYYY"));
 
+    
+    if (localStorage.getItem("day") !== day) {
+        localStorage.clear();
+        localStorage.setItem("day", day);
+}
 
 }
 
+// this function sets the time block color by changing the class of the time blocks to "past", "present", and future
 function timeBlockSet () {
     var currentHour = parseInt(dayjs().format("H"));
 
@@ -76,33 +79,12 @@ function timeBlockSet () {
     });
 }
 
+// this is a jQuery call that initiates the enclosed function after the page has rendered the html
 $(document).ready(function() {
 
     pressSave();
     dateSet();
     timeBlockSet();
     textSet();
-
     
 });
-
-// $(function () {
-//     // TODO: Add a listener for click events on the save button. This code should
-//     // use the id in the containing time-block as a key to save the user input in
-//     // local storage. HINT: What does `this` reference in the click listener
-//     // function? How can DOM traversal be used to get the "hour-x" id of the
-//     // time-block containing the button that was clicked? How might the id be
-//     // useful when saving the description in local storage?
-//     //
-//     // TODO: Add code to apply the past, present, or future class to each time
-//     // block by comparing the id to the current hour. HINTS: How can the id
-//     // attribute of each time-block be used to conditionally add or remove the
-//     // past, present, and future classes? How can Day.js be used to get the
-//     // current hour in 24-hour time?
-//     //
-//     // TODO: Add code to get any user input that was saved in localStorage and set
-//     // the values of the corresponding textarea elements. HINT: How can the id
-//     // attribute of each time-block be used to do this?
-//     //
-//     // TODO: Add code to display the current date in the header of the page.
-//   });
